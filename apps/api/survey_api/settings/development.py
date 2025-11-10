8a4a3d65df2eaa4c8e0c3f0e40aae0c69e2cab99
syamlal <syamlalms8@gmail.com>
2025-11-10 14:41:18 +0400

    hosting

--- apps/api/survey_api/settings/development.py
+++ apps/api/survey_api/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0','survey.task.energy']
 
 # CORS settings for local development
 INSTALLED_APPS += ['corsheaders']
