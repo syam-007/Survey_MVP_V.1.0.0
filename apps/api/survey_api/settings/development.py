fc4f74c1aa3b4bb3a419246eb961421229a7691d
syamlal <syamlalms8@gmail.com>
2025-11-11 13:24:56 +0400

    hosting-issue allowed host

--- apps/api/survey_api/settings/development.py
+++ apps/api/survey_api/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0','survey.task.energy']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0','survey.task.energy','3.28.242.100']
 
 # CORS settings for local development
 INSTALLED_APPS += ['corsheaders']
